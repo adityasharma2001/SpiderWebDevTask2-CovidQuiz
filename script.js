608a89c5fba449a949c986bfc5bc017c448f9b26
adityasharma2001 <aditshar2000@gmail.com>
2020-08-27 21:51:26 +0530

    added scoing system and gave final touch

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ const answerbtn = document.getElementById('answer-btn');
 const nextbtn = document.getElementById('next-btn');
 const questionElement = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
+const scoreShow = document.getElementById('score');
 let score = 0;
 
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
 nextbtn.addEventListener('click', next);
+previousbtn.addEventListener('click', previous);
 function next(){
   currentQuestionIndex++;
   nextQuestion();
+}
+function previous(){
+  currentQuestionIndex--;
+  previousQuestion();
 }
 
 function startGame(){
   console.log('Started');
   startButton.classList.add('hide');
+  scoreShow.classList.add('hide');
+  score = 0;
   shuffledQuestions = questions.sort(() => Math.random() - .5);
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove('hide');
@@ -46,6 +54,18 @@ function nextQuestion(){
 }
 
 function showQuestion(question){
+  if(currentQuestionIndex == 10){
+    questionContainerElement.classList.add('hide');
+    previousbtn.classList.add('hide');
+    answerbtn.classList.add('hide');
+    nextbtn.classList.add('hide');
+    startButton.classList.remove('hide');
+    startButton.innerText='RESTART';
+    scoreShow.classList.remove('hide');
+    scoreShow.innerText= 'SCORE: ' + score;
+
+  }
+  else{
   questionElement.innerText = question.question;
   question.answers.forEach(answer => {
     const button = document.createElement('button')
@@ -58,6 +78,7 @@ function showQuestion(question){
     answerButtons.appendChild(button)
   })
 }
+}
 
 function resetState(){
   while(answerButtons.firstChild){
@@ -66,6 +87,20 @@ function resetState(){
 }
 
 function previousQuestion(){
+  if(currentQuestionIndex == 9){
+    nextbtn.classList.add('hide');
+  }
+  else {
+    nextbtn.classList.remove('hide');
+  }
+  if(currentQuestionIndex == 0){
+    previousbtn.classList.add('hide');
+  }
+  else {
+    previousbtn.classList.remove('hide');
+  }
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 
 }
 
@@ -123,5 +158,57 @@ const questions = [
     { text: 'Bat' , correct: true},
     { text: 'Rat' , correct: false},
     { text: 'Cat' , correct: false}
+  ]},
+  {question: 'How many countries, areas or territories are suffering from novel coronavirus outbreak in the World?',
+  answers: [
+    { text: 'Less than 100' , correct: false},
+    { text: 'Less than 150' , correct: false},
+    { text: 'More than 200' , correct: true}
+  ]
+},
+  {question: 'In a study, which cells are found in COVID-19 patients bode well for long term immunity?',
+  answers:[
+    { text: 'P-cell' , correct: false},
+    { text: 'T-cell' , correct: true},
+    { text: 'D-cell' , correct: false}
+]},
+  {question: 'Name a clinical trial in which blood is transfused from recovered COVID-19 patients to a coronavirus patient who is in critical condition?',
+  answers:[
+    { text: 'Plasma Therapy' , correct: true},
+    { text: 'Hydroxychloroquine' , correct: false},
+    { text: 'Remdesivir' , correct: false}
+]},
+  {question: 'What is Corona Virus?',
+  answers:[
+    { text: 'It is a large family of viruses.' , correct: false},
+    { text: 'It belongs to the family of Nidovirus.' , correct: false},
+    { text: 'Both A and B are correct' , correct: true}
+  ]},
+  {question: 'Which of the following diseases are related to coronavirus?',
+  answers:[
+    { text: 'MERS' , correct: false},
+    { text: 'SARS' , correct: false},
+    { text: 'Both A and B' , correct: true}
+  ]},
+  {question: 'Mild Symptoms of Novel coronavirus are:',
+  answers:[
+    { text: 'Fever and Cough' , correct: false},
+    { text: 'Shortness of breath' , correct: false},
+    { text: 'All of the above' , correct: true}
+  ]},
+  {question: 'From where coronavirus got its name?',
+  answers:[
+    { text: 'Due to their crown-like projections.' , correct: true},
+    { text: 'Due to their leaf-like projections.' , correct: false},
+    { text: 'Due to their surface structure of bricks.' , correct: false}
+  ]},
+  {question: 'What are the precautions that need to be taken to protect from the coronavirus?',
+  answers:[
+    { text: 'Cover your nose and mouth when sneezing.' , correct: true},
+    { text: 'Add more garlic into your diet.' , correct: false},
+    { text: 'Visit your doctor for antibiotics treatment' , correct: false}
   ]}
+
+
+
 ]
